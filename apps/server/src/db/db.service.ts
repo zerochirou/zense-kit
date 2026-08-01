d80eadf7ec707a6b00e25f226d0a0d9d142d050d
Zerochirou <226718015+zerochirou@users.noreply.github.com>
2026-08-01 12:13:49 +0700

    fix: prisma client error

--- apps/server/src/db/db.service.ts
+++ apps/server/src/db/db.service.ts
@@ -2,11 +2,11 @@ import 'dotenv/config';
 
 import { Injectable, OnModuleDestroy } from '@nestjs/common';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from 'src/generated/prisma/client';
+import { PrismaClient } from '@prisma/client';
 
-const databaseUrl = process.env.DATABASE_URL;
+const databaseURL = process.env.DATABASE_URL;
 
-if (!databaseUrl) {
+if (!databaseURL) {
   throw new Error('DATABASE_URL belum ditentukan');
 }
 
@@ -14,7 +14,7 @@ if (!databaseUrl) {
 export class DbService extends PrismaClient implements OnModuleDestroy {
   constructor() {
     const adapter = new PrismaPg({
-      connectionString: databaseUrl,
+      connectionString: databaseURL,
     });
 
     super({ adapter });
